278918bc8d55533a228acf7f618815ffa21106a4
joaoAbracos <joaoabracos0@gmail.com>
2023-05-16 21:48:24 +0100

    create post validation

--- vue-front/src/views/Post/Create.vue
+++ vue-front/src/views/Post/Create.vue
@@ -8,14 +8,23 @@
         <div>
           <h4>Title</h4>
           <input type="text" v-model="model.post.title" />
+          <p v-if="validationErrors.title" class="error">
+            {{ validationErrors.title }}
+          </p>
         </div>
         <div>
           <h4>Body</h4>
           <input type="text" v-model="model.post.body" />
+          <p v-if="validationErrors.body" class="error">
+            {{ validationErrors.body }}
+          </p>
         </div>
         <div>
           <h4>Type</h4>
           <input type="text" v-model="model.post.type" />
+          <p v-if="validationErrors.type" class="error">
+            {{ validationErrors.type }}
+          </p>
         </div>
       </div>
       <button type="button" @click="savePost">Add</button>
@@ -50,8 +59,8 @@
 .main-title h2 {
   text-align: center;
 }
-.main-content h4{
-  margin : 5px 0;
+.main-content h4 {
+  margin: 5px 0;
 }
 .main-errors {
   display: flex;
@@ -71,6 +80,14 @@
   color: #fff;
   padding: 5px;
   font-size: 16px;
+}
+.error {
+  background-color: red;
+  padding: 5px;
+  margin: 0px;
+  color: #fff;
+  font-size: 12px;
+  text-align: center;
 }
 /* Make inputs responsive */
 .edit-posts input[type="text"] {
@@ -92,12 +109,10 @@
   border-radius: 4px;
   cursor: pointer;
   width: 100%;
-
+  margin-top: 10px;
 }
 .edit-posts button[type="button"]:hover {
   background-color: #0d771d;
-  
-
 }
 /* Style error messages */
 .edit-posts ul {
@@ -138,12 +153,20 @@ export default {
           type: "",
         },
       },
+      validationErrors: {
+        title: "",
+        body: "",
+        type: "",
+      },
     };
   },
   computed: {
     pageTitle() {
       const id = this.$route.params.postId;
       return id ? "Edit Post" : "Create Post";
+    },
+    isFormValid() {
+      return Object.values(this.validationErrors).every((error) => !error);
     },
   },
   created() {
@@ -170,29 +193,51 @@ export default {
           mythis.errorList = errors.response.data.errors;
         });
     },
+    verification(){
+      this.validationErrors = {
+        title: "",
+        body: "",
+        type: "",
+      };
+
+      // Perform field validation
+      if (!this.model.post.title) {
+        this.validationErrors.title = "Title is required";
+      }
+      if (!this.model.post.body) {
+        this.validationErrors.body = "Body is required";
+      }
+      if (!this.model.post.type) {
+        this.validationErrors.type = "Type is required";
+      }
+      return this.isFormValid
+    },
     savePost() {
-      const id = this.$route.params.postId;
+      
+      if (this.verification()) {
+        const id = this.$route.params.postId;
 
-      const createPost = axios.post;
-      const updatePost = axios.patch;
+        const createPost = axios.post;
+        const updatePost = axios.patch;
 
-      const apiUrl = "http://localhost:8000/api/v1/posts";
-      const url = id ? `${apiUrl}/${id}` : apiUrl;
-      const request = id ? updatePost : createPost;
+        const apiUrl = "http://localhost:8000/api/v1/posts";
+        const url = id ? `${apiUrl}/${id}` : apiUrl;
+        const request = id ? updatePost : createPost;
 
-      request(url, this.model.post)
-        .then((res) => {
-          this.model.post = {
-            title: "",
-            body: "",
-            type: "",
-          };
-          this.errorList = "";
-          this.$router.push("/posts");
-        })
-        .catch((errors) => {
-          this.errorList = errors.response.data.errors;
-        });
+        request(url, this.model.post)
+          .then((res) => {
+            this.model.post = {
+              title: "",
+              body: "",
+              type: "",
+            };
+            this.errorList = "";
+            this.$router.push("/posts");
+          })
+          .catch((errors) => {
+            this.errorList = errors.response.data.errors;
+          });
+      }
     },
   },
 };
